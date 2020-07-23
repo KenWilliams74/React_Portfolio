559bf5617d6ab02eb5da263de15cdf81775add38
Ken Williams <kenwilliams74@gmail.com>
2020-07-23 00:02:47 -0700

    Updates

---
+++ precache-manifest.ca24a81b1911bd2858414926472ddc3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd7597a641a17be1633412428bcc3b7",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "4d662579a4a84ce8129b",
+    "url": "/React_Portfolio/static/js/2.2ba41995.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React_Portfolio/static/js/2.2ba41995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "623c30cdcc189b861cbc",
+    "url": "/React_Portfolio/static/js/main.d136d9f8.chunk.js"
+  },
+  {
+    "revision": "3a0598dfa4517aaf1428",
+    "url": "/React_Portfolio/static/js/runtime-main.446e9683.js"
+  }
+]);
